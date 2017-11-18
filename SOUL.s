3ddd9c7bbc054916acfc0c89b33ef4eafc046b51
Daniel Pereira Ferragut <danielpferragut@gmail.com>
2017-11-18 15:27:50 -0200

    set_alarm terminado, comeco do tratamento de IRQ_HANDLER

--- SOUL.s
+++ SOUL.s
@@ -395,21 +395,34 @@ set_time:
 @	 -2 caso o tempo seja menor do que o tempo atual do sistema.
 @	  0 caso contrário.
 set_alarm:
-	mov r4, r0				@Ponteiro da funcao em R4
-	mov r5, r0				@Tempo do sistema desejado em R5
-
-	@Verifica se ha espaco para mais um alarme
-	ldr r0, =MAX_ALARMS
-	ldr r1, =ALARM_COUNTER
-	cmp r1, r0
-	moveq r0, #-1
-	beq SVC_fim
+	mov r4, r0					@Ponteiro da funcao em R4
+	mov r5, r0					@Tempo do sistema desejado em R5
 
 	@Verifica se o tempo do sistema é maior que o pedido
 	ldr r0, =CONTADOR
 	cmp r0, r5
 	movhi r0, #-2
 	bhi SVC_fim
+	
+	@Verifica se ha espaco para mais um alarme
+	ldr r0, =ALARM_COUNTER
+	ldr r1, =MAX_ALARMS
+	cmp r1, r0
+	moveq r0, #-1
+	beq SVC_fim
+
+
+	@Colocar novo alarme no vetor de structs de alarm
+	@R0 possui ALARM_COUNTER
+	ldr r1, =ALARM_ARRAY		@Carrega o comeco do vetor de structs em R1
+	str r4, [r1, r0, lsl #3]	@Coloca o ponteiro na struct
+	mov r4, r0					@R4 nao sera mais usado como ponteiro, agora eh ALARM_COUNTER
+	mov r0, r0, lsl #3			@Coloca em R0 a posicao do ponteiro armazenado
+	add r0, r0, #4				@Avanca 4 Bytes da posicao
+	str r5, [r1, r0]			@Armazena o tempo do sistema no final da struct
+	
+	add r4, r4, #1				@Apos a adicao do novo elemento no vetor, o ALARM_COUNTER sobe
+	mov r0, #0					@Operacao feita com sucesso, retorna R0=0	
 	b SVC_fim
 
 	@Voltar para o estado original do codigo
